2ad4e2cb33ca40a474bd63b6d8bc9d34b9603e97
Ian Lourenço <ianlourenco@protonmail.com>
2024-06-11 12:13:59 -0300

    fix: use correct tag for table data

--- src/components/ResultsTable.jsx
+++ src/components/ResultsTable.jsx
@@ -15,13 +15,13 @@ export default function ResultsTable({ userInput }) {
         </tr>
       </thead>
       <tbody>
-        {derivedResults.map((result, resultIndex) => (
-          <tr key={`${result}-${resultIndex}`}>
-            <th>{result.year}</th>
-            <th>{formatter.format(result.valueEndOfYear)}</th>
-            <th>{formatter.format(result.interest)}</th>
-            <th>{formatter.format(result.annualInvestment)}</th>
-            <th>{formatter.format(result.investedCapital)}</th>
+        {derivedResults.map((result) => (
+          <tr key={`${result.year}`}>
+            <td>{result.year}</td>
+            <td>{formatter.format(result.valueEndOfYear)}</td>
+            <td>{formatter.format(result.interest)}</td>
+            <td>{formatter.format(result.annualInvestment)}</td>
+            <td>{formatter.format(result.investedCapital)}</td>
           </tr>
         ))}
       </tbody>
